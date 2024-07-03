08b9cf375a6a3e9be4b8609f2f52701bbe13ac0d
Israel D. Canasa <rael@c-corp.dev>
2024-07-03 22:13:41 +0800

    Update mappings
    Disable arrow keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,8 +4,30 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- disable arrows
+map('n', '<Up>', ':echo "Use k"<CR>', { noremap = true, silent = true })
+map('n', '<Down>', ':echo "Use j"<CR>', { noremap = true, silent = true })
+map('n', '<Left>', ':echo "Use h"<CR>', { noremap = true, silent = true })
+map('n', '<Right>', ':echo "Use l"<CR>', { noremap = true, silent = true })
+
+map('i', '<Up>', '<Esc>:echo "Use k"<CR>', { noremap = true, silent = true })
+map('i', '<Down>', '<Esc>:echo "Use j"<CR>', { noremap = true, silent = true })
+map('i', '<Left>', '<Esc>:echo "Use h"<CR>', { noremap = true, silent = true })
+map('i', '<Right>', '<Esc>:echo "Use l"<CR>', { noremap = true, silent = true })
+
+map('v', '<Up>', ':echo "Use k"<CR>', { noremap = true, silent = true })
+map('v', '<Down>', ':echo "Use j"<CR>', { noremap = true, silent = true })
+map('v', '<Left>', ':echo "Use h"<CR>', { noremap = true, silent = true })
+map('v', '<Right>', ':echo "Use l"<CR>', { noremap = true, silent = true })
+
+-- Disable backspace in normal mode
+map('n', '<BS>', ':echo "Backspace is not allowed"<CR>', { noremap = true, silent = true })
+map('n', '<Del>', ':echo "Backspace is not allowed"<CR>', { noremap = true, silent = true })
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- toggle fold
 map("n", "<A-z>", "za")
 
 
